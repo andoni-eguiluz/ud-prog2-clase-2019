5ae62a4f724dab56dcb2b8abd5ed377189479fc3
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2019-02-04 20:49:22 +0100

    Primera clase

---
+++ Clase2019/src/HolaMundo.java
@@ -0,0 +1,17 @@
+
+/** Clase de inicio de Programación II
+ * Para honrar la generosidad y apertura que caracteriza a nuestra profesión,
+ * empezamos siempre cualquier experiencia de programación nueva 
+ * saludando al mundo
+ * @author andoni.eguiluz @ ingenieria.deusto.es
+ */
+public class HolaMundo {
+
+	/** Método principal de la clase
+	 * @param args	No utilizado
+	 */
+	public static void main(String[] args) {
+		System.out.println( "Hola mundo!" );
+	}
+
+}

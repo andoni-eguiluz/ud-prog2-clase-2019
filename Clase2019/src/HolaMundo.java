f8d1c38d58c2b03eed99edce78361e3b2c7f6b03
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2019-02-13 15:16:34 +0100

    Código de clase - introducción a Java

--- Clase2019/src/HolaMundo.java
+++ Clase2019/src/HolaMundo.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 /** Primera clase con pruebas básicas de sintaxis Java, variables, tipos
  * @author andoni.eguiluz @ ingenieria.deusto.es
  */
@@ -5,11 +7,13 @@ public class HolaMundo {
 	
 	public static void main(String[] args) {
 		System.out.println( "¡Hola y alegría mundo!" );  // \n \t \\
-		pruebasTipos();
+		pruebasTiposPrimitivos();
 	}
 	
-	private static void pruebasTipos() {
+	private static void pruebasTiposPrimitivos() {
+		//
 		// Primitivos: números, caracteres, boolean
+		//
 		byte b = 5;   // 1 byte
 		short s = 5;  // 2 bytes
 		int i = 5;    // 4 bytes
@@ -20,7 +24,7 @@ public class HolaMundo {
 		System.out.println( s + " máx " + Short.MAX_VALUE ); // Lo mismo en todas estas (short a string, int a string...)
 		System.out.println( i + " máx " + Integer.MAX_VALUE );
 		System.out.println( l + " máx " + Long.MAX_VALUE );
-		System.out.println( System.currentTimeMillis() );
+		System.out.println( System.currentTimeMillis() );  // Un ejemplo de long... que da para mucho
 		// System.out.println( new Date( 0 ) );   //  Si quisiéramos ver cómo se interpreta el long en una fecha
 		// System.out.println( new Date( Long.MAX_VALUE ) );  // Máxima fecha que se puede representar con longs (milisegundos desde el 1/1/1970)
 		System.out.println( f + " máx " + Float.MAX_VALUE );
@@ -29,6 +33,19 @@ public class HolaMundo {
 		boolean bo = true; // o false
 		System.out.println( "carácter " + c );
 		System.out.println( "booleano " + bo );
+		
+		//
+		// Todo lo demás en Java (que no sean estos 8 tipos primitivos) son objetos
+		//
+		// Hay dos muy particulares:  (su *new* puede ser implícito)
+		String st = "hola"; // new String("hola");
+		int[] array = { 2, 3, 4 }; // new int[] { 2, 3, 4 };
+		// (cada vez que hemos puesto un string en los printlns había news implícitos de string...
+		//  y cada vez que hemos hecho una concatenación, otro)
+		
+		// Pero el resto de objetos ya necesitan una construcción formal con el constructor de la clase:
+		Date fecha = new Date();
+		System.out.println( fecha );
 	}
 	
 }

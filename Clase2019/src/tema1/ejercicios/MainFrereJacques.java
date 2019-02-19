1fd40d39b9552103dc141bff0825cfe3d7c4ba4b
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2019-02-19 19:01:52 +0100

    Ejercicio de canción (1.9)

--- Clase2019/src/tema1/ejercicios/MainFrereJacques.java
+++ Clase2019/src/tema1/ejercicios/MainFrereJacques.java
@@ -16,6 +16,7 @@ public class MainFrereJacques {
 	}
 	
 	private static void tocaFrereJacques() {
+		boolean conAcompanyamiento = true;
 		// Crear canción
 		Cancion cancion = new Cancion( "Frere Jacques" );
 		cancion.setTempo( 2.0 );  // 2 segundos por cada tiempo
@@ -23,21 +24,23 @@ public class MainFrereJacques {
 				"1/8sol 1/8la 1/8sol 1/8fa mi do 1/8sol 1/8la 1/8sol 1/8fa mi do re sol3 1/2do re sol3 1/2do", 1, 4 );
 		System.out.println( "Canción: " + cancion );
 
-		/* Opción acompañamiento (acordes)
-		// Crear acompañamiento
-		Acompanyamiento acomp = new Acompanyamiento( "Frere Jacques - acordes" );
-		acomp.setTempo( 2.0 );
-		acomp.setOctavaDefecto( 3 );
-		acomp.addPartitura( "solM re4M 1/2solM solM re4M 1/2solM solM do4M 1/2re4M solM do4M 1/2re4M " +
-				"1/2re47 1/2solM 1/2re47 1/2solM do4M reM 1/2solM do4M reM 1/2solM", 1, 4 );
-		acomp.transponer( -7 );  // El acompañamiento está en sol y la melodía en do (disminuimos 7 semitonos el acompañamiento)
-		System.out.println( "Acompañamiento: " + acomp );
-		acomp.setVolumen( 0.2 );
-		 */
+		Acompanyamiento acomp = null;
+		if (conAcompanyamiento) {
+			// Crear acompañamiento (acordes)
+			acomp = new Acompanyamiento( "Frere Jacques - acordes" );
+			acomp.setTempo( 2.0 );
+			acomp.setOctavaDefecto( 3 );
+			acomp.addPartitura( "solM re4M 1/2solM solM re4M 1/2solM solM do4M 1/2re4M solM do4M 1/2re4M " +
+					"1/2re47 1/2solM 1/2re47 1/2solM do4M reM 1/2solM do4M reM 1/2solM", 1, 4 );
+			acomp.transponer( -7 );  // El acompañamiento está en sol y la melodía en do (disminuimos 7 semitonos el acompañamiento)
+			System.out.println( "Acompañamiento: " + acomp );
+			acomp.setVolumen( 0.2 );
+		}
 		
 		// Lanzar canción (y acompañamiento si existe al mismo tiempo)
 		cancion.play( 0 );  // Lanza canción por el canal 0
-//		acomp.play( new int[] { 1, 2, 3, 4 } );  // Y acompañamiento por los canales 1-4
+		if (conAcompanyamiento)
+			acomp.play( new int[] { 1, 2, 3, 4 } );  // Y acompañamiento por los canales 1-4
 		
 		Pianillo.closeCuandoSeAcabe();
 	}

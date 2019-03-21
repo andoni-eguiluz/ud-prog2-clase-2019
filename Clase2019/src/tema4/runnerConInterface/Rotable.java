126eb271b90b17c0b3d763bf7dd62f04d69ed235
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2019-03-21 10:05:52 +0100

    El primer interface

---
+++ Clase2019/src/tema4/runnerConInterface/Rotable.java
@@ -0,0 +1,8 @@
+package tema4.runnerConInterface;
+
+/** Comportamiento de rotación
+ * @author andoni.eguiluz @ ingenieria.deusto.es
+ */
+public interface Rotable {
+	public void rotar( double rot );
+}

1179f20715defe516aa335c4c57547ffe0e50299
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2019-05-10 11:38:54 +0200

    Mejora ejemplo revalidate con checkbox para ver qué pasa si se hace o no

--- Clase2019/src/temas78/ejemplosSwing/otros/EjemploCambioVentanaEnCaliente.java
+++ Clase2019/src/temas78/ejemplosSwing/otros/EjemploCambioVentanaEnCaliente.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-/** Ejemplo de cambio de ventana en caliente
+/** Ejemplo de cambio de ventana en caliente (revalidate)
  * @author andoni.eguiluz at deusto.es
  */
 @SuppressWarnings("serial")
@@ -15,21 +15,28 @@ public class EjemploCambioVentanaEnCaliente extends JFrame {
 	}
 	
 	JPanel pCentro = new JPanel();
+	JCheckBox cbConRevalidate = new JCheckBox( "Usar revalidate()", true );
 	int numBotones = 0;
+	
 	public EjemploCambioVentanaEnCaliente() {
 		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
 		setSize( 800, 640 );
+		JPanel pSuperior = new JPanel();
 		JButton bPrueba = new JButton( "Púlsame!" );
+		pSuperior.add( cbConRevalidate );
+		getContentPane().add( pSuperior, BorderLayout.NORTH );
+		getContentPane().add( bPrueba, BorderLayout.SOUTH );
+		getContentPane().add( pCentro, BorderLayout.CENTER );
 		bPrueba.addActionListener( new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				numBotones++;
 				JButton bNuevo = new JButton( "Botón nuevo " + numBotones );
 				pCentro.add( bNuevo );
-				pCentro.revalidate();  // Fundamental hacer esto cuando se cambia la estructura de un contenedor "en caliente"
+				if (cbConRevalidate.isSelected()) {
+					pCentro.revalidate();  // Fundamental hacer esto cuando se cambia la estructura de un contenedor "en caliente"
+				}
 			}
 		});
-		getContentPane().add( bPrueba, BorderLayout.SOUTH );
-		getContentPane().add( pCentro, BorderLayout.CENTER );
 	}
 }

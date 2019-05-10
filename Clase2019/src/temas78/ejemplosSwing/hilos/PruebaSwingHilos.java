58168c8dc4e55dc716de80e05b24c5197a17b006
Andoni Eguíluz <andoni.eguiluz@deusto.es>
2019-05-10 12:05:02 +0200

    Mejora ejemplo de hilos

--- Clase2019/src/temas78/ejemplosSwing/hilos/PruebaSwingHilos.java
+++ Clase2019/src/temas78/ejemplosSwing/hilos/PruebaSwingHilos.java
@@ -1,11 +1,13 @@
 package temas78.ejemplosSwing.hilos;
 
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+/** Prueba de problemas de Swing sin usar hilos (¿cómo se cambiaría para que funcione?)
+ * @author andoni.eguiluz at ingenieria.deusto.es
+ */
 public class PruebaSwingHilos {
 	public static void main(String[] args) {
 		JFrame v = new JFrame();
@@ -18,7 +20,10 @@ public class PruebaSwingHilos {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
-					Thread.sleep( 15000 );  // Esto va a traer problemas...  :-)
+					for (int i=0;i<10;i++) {  // Esto va a traer problemas...  :-)
+						System.out.println( "Estoy trabajando... " + i + " segundos");
+						Thread.sleep( 1000 );
+					}
 				} catch (Exception ex) {}
 			}
 		});
